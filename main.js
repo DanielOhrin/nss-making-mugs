605498e9d396d8a67bda41ea1b3eee0123f33e2a
Daniel Ohrin <xqiam@outlook.com>
2022-10-07 09:27:34 -0500

    Initial

---
+++ main.js
@@ -0,0 +1,9 @@
+const clay = [ "Clay", "Clay", "Clay", "Clay" ];
+const toFireInKiln = [];
+
+for (let a in clay) {
+   const mug = `${clay[a]} coffee mug`;
+   toFireInKiln.push(mug);
+}
+
+console.log(toFireInKiln);
